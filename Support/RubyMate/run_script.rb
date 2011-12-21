845be06b720c3e9e04eedfff00427cba5bf54e51
John Berry <jberry+githubcommits@lumoslabs.com>
2011-12-21 11:55:49 -0800

    Use String#each_line to get an Enumerable, since String is no longer Enumerable in Ruby 1.9.

--- Support/RubyMate/run_script.rb
+++ Support/RubyMate/run_script.rb
@@ -40,7 +40,7 @@ class RubyMate < ScriptMate
             "<br style=\"display: none\"/>"
     else
       if @command.test_script?
-        return ( str.map do |line|
+        return ( str.each_line.map do |line|
           if line =~ /^(\s+)(\S.*?):(\d+)(?::in\s*`(.*?)')?/
             indent, file, line, method = $1, $2, $3, $4
             url, display_name = '', 'untitled document';
